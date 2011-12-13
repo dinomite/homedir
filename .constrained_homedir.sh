dc49ccacf135d8b5db9938745688ac71b9ab023d
Drew Stephens <drew@dinomite.net>
2011-12-13 15:23:43 -0500

    Proper name for .constrained_homedir.sh

--- .constrained_homedir.sh
+++ .constrained_homedir.sh
@@ -6,7 +6,7 @@ GITHUB_FILES=( .bash_aliases .bash_functions .bash_global .bashrc .vimrc.simple 
 # Files that need sed run on them to insert CONSTRAIN_HOMEDIR in paths they reference
 SED_TRANSFORM_FILES=( .bashrc .shell )
 # Files that should be made executable
-CHMOD_X_FILES=( .constrained_homedir .shell )
+CHMOD_X_FILES=( .constrained_homedir.sh .shell )
 
 # See: http://noobody.org/Data/PonychanImageCrawler.bash
 function depCheck()

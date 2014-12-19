7668ec0f06d0f111445234a68796a293d71ddc19
Drew Stephens <drew@dinomite.net>
2014-12-19 12:33:11 -0500

    Update for torrents location

--- bin/copytorrents.sh
+++ bin/copytorrents.sh
@@ -1,4 +1,4 @@
 if test -n "$(find ~/Downloads -maxdepth 1 -name '*.torrent' -print -quit)"
 then
-    scp ~/Downloads/*.torrent dinomite@192.168.2.100:/mnt/pescennius/Torrents/NEW-TORRENTS/ && rm ~/Downloads/*.torrent
+    scp ~/Downloads/*.torrent dinomite@192.168.2.100:/mnt/pescennius/NewTorrents/ && rm ~/Downloads/*.torrent
 fi

21d20a1d16459e299989632795821e3dd8a4e2d1
Drew Stephens <drew@dinomite.net>
2015-05-25 08:04:03 -0400

    chmod torrents before transferring

--- bin/copytorrents.sh
+++ bin/copytorrents.sh
@@ -1,4 +1,6 @@
+TORRENT_PATH=~/Downloads/*.torrent
 if test -n "$(find ~/Downloads -maxdepth 1 -name '*.torrent' -print -quit)"
 then
-    scp -p ~/Downloads/*.torrent dinomite@192.168.2.100:/mnt/pescennius/NewTorrents/ && rm ~/Downloads/*.torrent
+    chmod 664 $TORRENT_PATH
+    scp -p $TORRENT_PATH dinomite@192.168.2.100:/mnt/pescennius/NewTorrents/ && rm ~/Downloads/*.torrent
 fi

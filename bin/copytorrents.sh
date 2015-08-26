a862bcacb13bf6b7d2fddeb0643bf2757f288672
Drew Stephens <drew@dinomite.net>
2015-08-26 11:34:42 -0400

    Use hostname for copytorrents

--- bin/copytorrents.sh
+++ bin/copytorrents.sh
@@ -2,5 +2,5 @@ TORRENT_PATH=~/Downloads/*.torrent
 if test -n "$(find ~/Downloads -maxdepth 1 -name '*.torrent' -print -quit)"
 then
     chmod 664 $TORRENT_PATH
-    scp -p $TORRENT_PATH dinomite@192.168.2.100:/mnt/pescennius/NewTorrents/ && rm ~/Downloads/*.torrent
+    scp -p $TORRENT_PATH dinomite@septimius.local:/mnt/pescennius/NewTorrents/ && rm ~/Downloads/*.torrent
 fi

7e6cc309b76fd9715644f525c3ad57d28b0c9276
Drew Stephens <drew@dinomite.net>
2018-06-26 06:21:53 -0400

    Open Synergy hidden

--- bin/copytorrents.sh
+++ bin/copytorrents.sh
@@ -2,5 +2,5 @@ TORRENT_PATH=~/Downloads/*.torrent
 if test -n "$(find ~/Downloads -maxdepth 1 -name '*.torrent' -print -quit)"
 then
     chmod 664 $TORRENT_PATH
-    scp -p $TORRENT_PATH dinomite@septimius:/mnt/pescennius/NewTorrents/ && rm ~/Downloads/*.torrent
+    scp -p $TORRENT_PATH dinomite@didius:/mnt/pescennius/NewTorrents/ && rm ~/Downloads/*.torrent
 fi

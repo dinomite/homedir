8314c2b4c509ab7824a41ba05814521059f3f809
Drew Stephens <drew@dinomite.net>
2020-01-02 20:32:06 +0000

    Script to backup emoncms DB

---
+++ bin/emoncms-db-backup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Backup the emoncms DB
+set -e
+
+BACKUP_DIR=/mnt/pescennius/backups/emoncms
+CONTAINER=$(docker ps | grep emoncms.*db | awk '{print $1}')
+FILE_SUFFIX=".sql"
+FILE_NAME=${CONTAINER}-$(date +"%Y-%m-%d")${FILE_SUFFIX}
+docker exec e7017f1a4ccf /usr/bin/mysqldump -u emoncms --password=YOUR_SECURE_PASSWORD emoncms > ${BACKUP_DIR}/${FILE_NAME}
+
+date
+
+find ${BACKUP_DIR} -type f -mtime +0 -name '*.${FILE_SUFFIX}' -delete

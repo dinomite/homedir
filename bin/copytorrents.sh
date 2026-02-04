8e94a30a252149c21433258881ceeb35736a9cbe
Drew Stephens <drew@dinomite.net>
2026-02-03 19:28:15 -0500

    Update little things

--- bin/copytorrents.sh
+++ bin/copytorrents.sh
@@ -2,5 +2,6 @@ TORRENT_PATH=~/Downloads/*.torrent
 if test -n "$(find ~/Downloads -maxdepth 1 -name '*.torrent' -print -quit)"
 then
     chmod 664 $TORRENT_PATH
-    scp -p $TORRENT_PATH dinomite@hostilian:/mnt/pescennius/Torrents/.new_torrents/ && rm ~/Downloads/*.torrent
+    #scp -p $TORRENT_PATH dinomite@hostilian:/mnt/pescennius/Torrents/.new_torrents/ && rm ~/Downloads/*.torrent
+    cp $TORRENT_PATH /Volumes/media/torrents/new-torrents/ && rm ~/Downloads/*.torrent
 fi
